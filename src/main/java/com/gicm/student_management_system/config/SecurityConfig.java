f945b1a9abe9bf5638eed428d1db9875d3e626ed
Pyae Phyo Kyaw <pyaephyokyaw@gicjp.com>
2025-12-03 15:31:40 +0630

    Create SecurityConfig.java

---
+++ src/main/java/com/gicm/student_management_system/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.gicm.student_management_system.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+
+}
